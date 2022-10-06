6f208259bd0d59ef761a0e0c4611cc4eaa9bad5b
smorabit <smorabit82@gmail.com>
2022-10-06 14:37:20 -0700

    MetaspotsByGroups bugfix

--- R/ConstructMetaspots.R
+++ R/ConstructMetaspots.R
@@ -20,6 +20,11 @@ ConstructMetaspots <- function(
   # get expression matrix:
   X <- GetAssayData(cur_seurat, slot='counts')
 
+  # check to make sure this is just one sample:
+  if(sum(unlist(lapply(Images(cur_seurat), function(x){nrow(cur_seurat@images[[x]]@coordinates) == 0}))) != 1){
+    stop("More than one sample present in grouping. Please specify a metadata column with group.by indicating different ST samples.")
+  }
+
   # boundaries
   row_range <- range(cur_seurat$row)
   col_range <- range(cur_seurat$col)
